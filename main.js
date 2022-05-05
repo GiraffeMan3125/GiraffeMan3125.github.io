a38c674d00a4901c58cd3e9422a9bc0f62af509b
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-05-05 10:16:05 -0400

    maybe this will work??

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var camera = document.querySelector('a-entity[camera]').components.camera.camera;
 
 while (true) {
-    camera.rotation.x++;
+    cameras.getAttribute('rotation');
+    el.setAttribute('rotation', {x: i});
+    i++;
 }
