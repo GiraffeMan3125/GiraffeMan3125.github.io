6774d7f53588bda53a31a580eb5632e2104141b9
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-05-09 09:39:47 -0400

    maybe reverse mouse drag works now?

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var camera = document.querySelector('a-entity[camera]').components.camera.camera
 camera.setAttribute('reverseMouseDrag', true);
 
 while (true) {
-    camera.getAttribute('rotation');
-    camera.setAttribute('rotation', {x: i});
+    camera.getAttribute('scale');
+    camera.setAttribute('scale', {x: i});
     i++;
 };
