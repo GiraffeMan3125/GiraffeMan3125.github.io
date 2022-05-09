13d0ff528bff72de8ae37bf17f873ef84aed0775
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-05-09 10:28:16 -0400

    ok now using ticks, so maybe it will work

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// var camera = document.querySelector('#camera')
-
-// camera.setAttribute('look-controls', 'reverseMouseDrag', true);
-
-// var a = 0
-
+// Old Unworking Code
 // while (true) {
 //     camXScale = camera.getAttribute('scale');
-//     camera.setAttribute('scale', {x: (a + camXScale)});
-//     a++;
+//     camera.setAttribute('scale', {x: (1 + camXScale)});
 // };
+
+AFRAME.registerComponent('scaleGoBrr', {
+    tick: function () {
+        // `this.el` is the element.
+        // `object3D` is the three.js object.
+    
+        // `rotation` is a three.js Euler using radians. `quaternion` also available.
+        //   console.log(this.el.object3D.rotation);
+    
+        //   `position` is a three.js Vector3.
+        //   console.log(this.el.object3D.position);
+        this.el.object3D.scale = this.el.object3D.scale - 1
+    }
+  });
