802d14ca66b6e5633e65a0329a3f5f3c5a6f67ff
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-05-17 10:04:12 -0400

    lol fake sleep function HAHAHA

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-AFRAME.registerComponent('a', {
-    tick: function () {
-        // `this.el` is the element.
-        // `object3D` is the three.js object.
-    
-        // `rotation` is a three.js Euler using radians. `quaternion` also available.
-        //   console.log(this.el.object3D.rotation);
-    
-        //   `position` is a three.js Vector3.
-        //   console.log(this.el.object3D.position);
-        console.log(this.el.object3D.position);
-    }
-  });
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+  
 
-AFRAME.registerComponent('moveonclick', {
-    init: function () {
-    var el = this.el;        
-    el.addEventListener('click', function () {   
-        console.log("test")         
-        el.object3D.scale = '5624 943015 648392'
-    });        
+AFRAME.registerComponent('cets', {
+    play: function () {
+         while (True) {
+            this.el.object3D.position.x += 1
+            sleep(500)
+         }
+
     }
 });
