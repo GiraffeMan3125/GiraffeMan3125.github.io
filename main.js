2a8ada8c90224ffeafef6f320ece86bfa5e167fb
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-05-19 10:31:32 -0400

    tucf

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 // WHY WONT MY CODE WORK AAAAA
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-        currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+const words = [
+    "Virtual Reality, and it's use\n in treating Mental Illness", 
+    "I need a second line of words\n to make sure this works.",
+    "A 3rd line is always nice."
+]
+var i = 1;                  //  set your counter to 1
+
+function loopychangetextboy() {
+  setTimeout(function() {
+    console.log("test");
+    while (this.opa > 0) {
+        this.opacity - 0.05;
+    }
+    i++;
+    if (i < words.length) {
+        loopychangetextboy();
+    }
+  }, 500)
 }
-  
 
-AFRAME.registerComponent('cets', {
+loopychangetextboy();
+
+AFRAME.registerComponent("cets", {
     init: function () {
-         while (True) {
-            console.log("test");
-            this.el.object3D.position.x += 1;
-            sleep(500);
-         }
-
+        while (True) {
+            loopychangetextboy();
+        }
     }
 });
