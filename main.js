0877a14fcb446586d14901aa158b6cb0eabf80e3
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-06-07 09:46:33 -0400

    cfughmbn

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ var i = 1;
 
 async function loopychangetextboy() {
     await new Promise(r => setTimeout(r, 5000));
-    alert(this.text.opacity)
-    while (this.text.opacity > 0) {
-        this.text.opacity = this.text.opacity - 0.05;
+    alert(this.opacity)
+    while (this.opacity > 0) {
+        this.opacity = this.opacity - 0.05;
     }
     i++;
     this.value = words[i];
-    while (this.text.opacity < 1) {
-        this.text.opacity = this.text.opacity + 0.05;
+    while (this.opacity < 1) {
+        this.opacity = this.opacity + 0.05;
     }
     if (i < words.length) {
         loopychangetextboy();
