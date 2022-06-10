2efdac7cf3d137e3c1eed50f65ac81a27ebc80b1
GiraffeMan <30411273+GiraffeMan3125@users.noreply.github.com>
2022-06-10 18:33:53 -0400

    zasxedcrfvgbhnjmk

--- main2electricboogaloo.js
+++ main2electricboogaloo.js
@@ -15,7 +15,8 @@ AFRAME.registerComponent('maincomp', {
 
 function run() {
     alert("ran");
-    text.setAttribute("text", "value", "a");
+    text.setAttribute('text', {value: 'a'});
+    // entityEl.setAttribute('light', {color: '#ACC', intensity: 0.75});
     alert("changed text");
 //     while (i < words.length) {
 //         await new Promise(r => setTimeout(r, 5000));
